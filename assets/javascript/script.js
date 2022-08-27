a05df1df56cbe90d991561af251e6a6cd4c8bfbd
TDiaz83 <tomas.enrique.diaz@gmail.com>
2022-08-27 18:20:53 -0500

    got Font Awesome Save Icon to preferred size

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -29,7 +29,7 @@ function createTimeBlocks() {
         //create TextArea
         $("#hour"+x).append('<textarea class="col-sm-10 description"></textarea>');
         //create save icon
-        $("#hour"+x).append('<button class="col-sm-1 saveBtn btn"><i class="far fa-save fa-lg"></i></button>');
+        $("#hour"+x).append('<button class="col-sm-1 saveBtn btn"><i class="fa fa-save fa-2x"></i></button>');
         if (i < current) {
             $( "#hour"+ x + " textarea" ).addClass("past");
         } else if (i == current) {
