00f256da8c60ba3470f8bd218d2b5091d732b7b9
TDiaz83 <tomas.enrique.diaz@gmail.com>
2022-08-28 19:33:35 -0500

    All working, should have committed more, but did not...

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,17 +1,35 @@
 //aliasing the luxon class
 var DateTime = luxon.DateTime;
+var dt = DateTime.now().toFormat('cccc, dd MMMM');
 
-//setting date
-document.querySelector("#currentDay").textContent = DateTime.now().toFormat('cccc, dd MMMM');
+//creating objects of tasks
+var tasks = {
+    hour08 : "",
+    hour09 : "",
+    hour10 : "",
+    hour11 : "",
+    hour12 : "",
+    hour13 : "",
+    hour14 : "",
+    hour15 : "",
+    hour16 : "",
+    hour17 : "",
+}
 
-//current hour
+//obtaining stored tasks
+var storedTasks = JSON.parse(localStorage.getItem(""+dt+""));
+console.log(storedTasks);
+
+//filling task object if stored
+if (storedTasks) {
+    Object.assign(tasks, storedTasks);
+}
+
+//setting current date element
+document.querySelector("#currentDay").textContent = dt;
+
+//setting current hour
 var current = DateTime.now().toFormat('H');
-console.log(current);
-
-//Create timeblocks
-    //For loop for 9-5
-    //condition for past/present/future
-    //make textarea clickable, receive/submit
 
 //creating the time blocks from 9-5
 function createTimeBlocks() {
@@ -27,9 +45,11 @@ function createTimeBlocks() {
         //create hour block
         $("#hour"+x).append("<div class='col-sm-1 hour'>"+DateTime.fromISO(x+':00').toFormat("h:mm a").toLowerCase()+"</div>");
         //create TextArea
-        $("#hour"+x).append('<textarea class="col-sm-10 description"></textarea>');
+        $("#hour"+x).append('<textarea class = "col-sm-10 description"></textarea>');
+        //fill task list from storage
+        $("#hour"+x+" textarea").val(tasks["hour"+x+""]);
         //create save icon
-        $("#hour"+x).append('<button class="col-sm-1 saveBtn btn"><i class="fa fa-save fa-2x"></i></button>');
+        $("#hour"+x).append('<button class = "btn col-sm-1 saveBtn"><i class="fa fa-save fa-2x"></i></button>');
         if (i < current) {
             $( "#hour"+ x + " textarea" ).addClass("past");
         } else if (i == current) {
@@ -40,9 +60,16 @@ function createTimeBlocks() {
     }
 }
 
+//make the scheduler blocks
 createTimeBlocks();
 
-//Fill timeblocks from local storage
-//Color time blocks for past/present/future
-//Allow addition of text/tasks to timeblockks
-//Save text/task to local storage
+//click the button to save a task
+$("button").on("click", function(e) {
+    e.preventDefault();
+    var hour = $(this).parent().attr("id");
+    var newTasks = $("#"+hour).children().eq(1).val();
+    console.log(hour);
+    console.log(newTasks);
+    tasks[hour] = newTasks;
+    localStorage.setItem(dt, JSON.stringify(tasks));
+});
